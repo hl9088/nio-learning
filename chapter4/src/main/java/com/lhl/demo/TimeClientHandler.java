0a377f3f21e5789d192f7a7c636d43f874ca7ed5
lihongli <lihongli@thinkit.cn>
2019-01-10 14:34:58 +0800

    第四章 粘包问题

---
+++ chapter4/src/main/java/com/lhl/demo/TimeClientHandler.java
@@ -0,0 +1,47 @@
+package com.lhl.demo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+
+/**
+ * Created by lihongli on 2019/1/10
+ */
+public class TimeClientHandler extends ChannelHandlerAdapter {
+
+    private int counter;
+
+    private byte[] req;
+
+    public TimeClientHandler() {
+        req = ("QUERY TIME ORDER" + System.getProperty("line.separator")).getBytes();
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ByteBuf message = null;
+        for (int i = 0; i < 100; i++) {
+            message = Unpooled.buffer(req.length);
+            message.writeBytes(req);
+            ctx.writeAndFlush(message);
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] request = new byte[buf.readableBytes()];
+        buf.readBytes(request);
+        String body = new String(request, "UTF-8");
+        System.out.println("Now is : " + body + " ; the counter is : " + ++counter);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        // 释放资源
+        ctx.close();
+    }
+}
